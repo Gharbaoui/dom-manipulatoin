5807b76fe97fa9c7b10e7573af1afb467f879afe
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-30 14:02:10 +0100

    this will work with all li inside ul

--- src/events/delegation/app.js
+++ src/events/delegation/app.js
@@ -1,1 +1,16 @@
 // event delegation
+
+/*
+    it allows users to append a single event listener to a parent element
+    that adds it to all its present and future desendencts
+*/
+
+
+document.querySelector(`#sports`).addEventListener(`click`, (e) => {
+    console.log(`you have clicked on ${e.target.getAttribute(`id`)}`);
+
+    if (e.target.matches(`li`))
+    {
+        e.target.style.backgroundColor = "red";
+    }
+});
