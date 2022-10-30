ff6411d47409a7deeac0e2b18cccd6a40369ed0e
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-30 13:28:36 +0100

    multiple functionality
    stopPropagation will stop at that point
    and true value will tell that event should go from top down the tree

--- src/events/probagation/app.js
+++ src/events/probagation/app.js
@@ -1,26 +1,31 @@
 window.addEventListener(`click`, () => {
     console.log(`window clicked`);
-}, false);
+}, true);
 
 document.addEventListener(`click`, () => {
     console.log(`document`);
-}, false);
+}, true);
 
 document.querySelector(`.box-3`).addEventListener(`click`, () => {
     console.log(`box-3`);
-}, false);
+}, true);
 
-document.querySelector(`.box-2`).addEventListener(`click`, () => {
+document.querySelector(`.box-2`).addEventListener(`click`, (e) => {
+
+    e.stopPropagation();
     console.log(`box-2`);
-}, false);
+}, true);
 
 document.querySelector(`.box-1`).addEventListener(`click`, () => {
     console.log(`box-1`);
-}, false);
+}, true);
 
 document.querySelector(`.btn`).addEventListener(`click`, (e) => {
     console.log(`button clicked`);
     console.log(e);
-}, false);
+}, true);
 
 // event probagation is how event should accur order should event go up the dom tree or down by default it goes up
+// false is going up the dom tree 
+// true is going from top to button
+// we can stop it at some point if we want to
