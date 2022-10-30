dfbc739f552bb30f9981bce120d698cc6c70e5bb
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-30 10:12:42 +0100

    select by tag, id, class

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 // How to select element
 
 const headTitle = document.getElementById(`main-heading`);
-console.log(headTitle);
+// console.log(headTitle);
 // How to get all elements that match a class name
 
 const items = document.getElementsByClassName(`list-items`);
-console.log(items[0]);
+// console.log(items[0]);
 
 const liTags = document.getElementsByTagName(`li`);
-console.log(liTags);
+// console.log(liTags);
+
+
+// it will return the first match
+const getByAnything = document.querySelector(`div`);
+console.log(getByAnything);
+
+const getByAnythingAll = document.querySelectorAll(`div`);
+console.log(getByAnythingAll);
