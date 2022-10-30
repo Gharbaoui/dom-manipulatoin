0f9721c0d3bfc14db47f8ca2e0c1b4c73f2c5698
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-30 10:07:36 +0100

    common ways to get elements

--- src/app.js
+++ src/app.js
@@ -2,3 +2,10 @@
 
 const headTitle = document.getElementById(`main-heading`);
 console.log(headTitle);
+// How to get all elements that match a class name
+
+const items = document.getElementsByClassName(`list-items`);
+console.log(items[0]);
+
+const liTags = document.getElementsByTagName(`li`);
+console.log(liTags);
